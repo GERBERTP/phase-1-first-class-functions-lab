ba3cf854a9ccb11b85eba9e4d32fe684ca223e21
Gerbert Pesquera <gerbert.pesquera@gmail.com>
2022-02-08 00:25:45 -0500

    commit tree cfa02b212079f235647b63d856d78ddbbf0f007a

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+// Code
+const returnFirstTwoDrivers = function (drivers){
+    return drivers.slice(0,2)
+};
+
+
+const returnLastTwoDrivers = function (drivers){
+    return drivers.slice(2)
+};
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+const createFareMultiplier = function (fare){
+    return function (fareQuintupler){
+        return fare * fareQuintupler
+    };
+};
+
+const fareDoubler = function (fare){
+    return fare * (2)
+}; 
+
+const fareTripler = function (fare){
+    return fare * (3)
+};
+
+const selectDifferentDrivers = function (drivers, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(drivers)
+};
